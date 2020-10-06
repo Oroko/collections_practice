0428c66a4cfc26a2077c09036a9e448f023ccf13
Oroko <aoroko09@gmail.com>
2020-10-06 17:53:54 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
+def sort_array_asc(array)
+  array.sort
+end
+def sort_array_desc(array)
+  array.sort.reverse
+end
+def sort_array_char_count(array)
+  array.sort_by do |word|
+    word.length
+  end
+end
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+def reverse_array(array)
+ array.reverse!
+ array
+end
+def kesha_maker(array)
+  array.map do |word|
+    word[2] = "$"
+    
+  end
+  array
+end
+
+def find_a(array)
+  array.select do |word|
+     if word[0] == "a"
+      word
+     end
+    end
+  
+end
+
+def sum_array(array)
+  sum = 0
+  array.map do |num|
+    sum += num
+  end
+  sum
+end
+
+def add_s(array)
+  array.reject{|word|  word == array[1]}.map do |word|
+    word << "s"
+  end
+  array
+end
 
